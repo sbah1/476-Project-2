5f0d3a443b70a1e7af9ece0cb6daf17f37dc8d2f
Sabba7h <56412329+sbah1@users.noreply.github.com>
2022-03-29 00:52:46 -0400

    Initial commit

---
+++ Length Converter.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace _476_Project_2
+{
+    partial class Length_Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label12 = new System.Windows.Forms.Label();
+            this.bruhsoundhashtag2 = new System.Windows.Forms.TextBox();
+            this.A_LIST = new System.Windows.Forms.ComboBox();
+            this.emoji_45 = new System.Windows.Forms.Button();
+            this.numbers = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numbers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(135, 35);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 13);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Kilometer";
+            // 
+            // bruhsoundhashtag2
+            // 
+            this.bruhsoundhashtag2.Location = new System.Drawing.Point(29, 58);
+            this.bruhsoundhashtag2.Name = "bruhsoundhashtag2";
+            this.bruhsoundhashtag2.ReadOnly = true;
+            this.bruhsoundhashtag2.Size = new System.Drawing.Size(100, 20);
+            this.bruhsoundhashtag2.TabIndex = 2;
+            this.bruhsoundhashtag2.Text = "NaN";
+            // 
+            // A_LIST
+            // 
+            this.A_LIST.FormattingEnabled = true;
+            this.A_LIST.Items.AddRange(new object[] {
+            "Mile",
+            "Yard",
+            "Foot"});
+            this.A_LIST.Location = new System.Drawing.Point(135, 58);
+            this.A_LIST.Name = "A_LIST";
+            this.A_LIST.Size = new System.Drawing.Size(121, 21);
+            this.A_LIST.TabIndex = 3;
+            this.A_LIST.Text = "Mile";
+            // 
+            // emoji_45
+            // 
+            this.emoji_45.Location = new System.Drawing.Point(95, 85);
+            this.emoji_45.Name = "emoji_45";
+            this.emoji_45.Size = new System.Drawing.Size(75, 23);
+            this.emoji_45.TabIndex = 4;
+            this.emoji_45.Text = "Convert";
+            this.emoji_45.UseVisualStyleBackColor = true;
+            this.emoji_45.Click += new System.EventHandler(this.emoji_45_Click);
+            // 
+            // numbers
+            // 
+            this.numbers.DecimalPlaces = 9;
+            this.numbers.Location = new System.Drawing.Point(29, 32);
+            this.numbers.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numbers.Minimum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            -2147483648});
+            this.numbers.Name = "numbers";
+            this.numbers.Size = new System.Drawing.Size(100, 20);
+            this.numbers.TabIndex = 5;
+            // 
+            // Length_Converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 126);
+            this.Controls.Add(this.numbers);
+            this.Controls.Add(this.emoji_45);
+            this.Controls.Add(this.A_LIST);
+            this.Controls.Add(this.bruhsoundhashtag2);
+            this.Controls.Add(this.label12);
+            this.Name = "Length_Converter";
+            this.Text = "Length Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.numbers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox bruhsoundhashtag2;
+        private System.Windows.Forms.ComboBox A_LIST;
+        private System.Windows.Forms.Button emoji_45;
+        private System.Windows.Forms.NumericUpDown numbers;
+    }
+}
+
---
+++ Length Converter.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _476_Project_2
+{
+    public partial class Length_Converter : Form
+    {
+        public abstract class converter
+        {
+            protected decimal num;
+            protected converter c;
+
+            public void setValue(decimal i)
+            {
+                num = i;
+            }
+
+            public decimal getValue()
+            {
+                return num;
+            }
+            public void addSuccessor(converter new_)
+            {
+                c = new_;
+            }
+
+            public abstract void Convert_(decimal distance);
+        }
+
+        public class Mile : converter
+        {
+            public Mile()
+            {
+                num = 0;
+            }
+            public override void Convert_(decimal distance)
+            {
+                num = decimal.Multiply(distance, Convert.ToDecimal(0.621371192));
+
+                if (c != null)
+                {
+                    c.Convert_(num);
+                }
+            }
+        }
+
+        public class Yard : converter
+        {
+            public Yard()
+            {
+                num = 0;
+            }
+            public override void Convert_(decimal distance)
+            {
+                num = decimal.Multiply(distance, Convert.ToDecimal(1760));
+
+                if (c != null)
+                {
+                    c.Convert_(num);
+                }
+            }
+        }
+
+        public class Feet : converter
+        {
+            public Feet()
+            {
+                num = 0;
+            }
+            public override void Convert_(decimal distance)
+            {
+                num = decimal.Multiply(distance, Convert.ToDecimal(3));
+
+                if (c != null)
+                {
+                    c.Convert_(num);
+                }
+            }
+        }
+
+        public class wrapper
+        {
+            protected decimal num;
+            protected decimal rounded;
+            protected string expNotation;
+            protected string fullString;
+            protected wrapper w;
+
+            public wrapper()
+            {
+                num = 0;
+            }
+
+            public decimal getNum()
+            {
+                return num;
+            }
+            public void setNum(decimal n)
+            {
+                num = n;
+            }
+
+            public decimal getRounded()
+            {
+                return rounded;
+            }
+
+            public string getExpNotation()
+            {
+                return expNotation;
+            }
+            public void generateExpNotation()
+            {
+                int counter = 0;
+
+                while (rounded > 10)
+                {
+                    rounded *= Convert.ToDecimal(0.1);
+                    counter++;
+                }
+
+                expNotation = Convert.ToString(rounded) + "e" + Convert.ToString(counter);
+            }
+            public string generateFullString(int index)
+            {
+                switch (index)
+                {
+                    case 0:
+                        fullString = expNotation + " Miles";
+                        break;
+                    case 1:
+                        fullString = expNotation + " Yards";
+                        break;
+                    case 2:
+                        fullString = expNotation + " Feet";
+                        break;
+                }
+
+                return fullString;
+            }
+        }
+
+        public class RoundDecimal : wrapper
+        {
+            public RoundDecimal(wrapper newWrapper)
+            {
+                this.w = newWrapper;
+                num = w.getNum();
+                rounded = decimal.Round(num, 2);
+            }
+        }
+
+        public class expNotation : wrapper
+        {
+            public expNotation(wrapper newWrapper)
+            {
+                this.w = newWrapper;
+                num = w.getNum();
+                rounded = w.getRounded();
+                expNotation = "";
+            }
+        }
+        public class fullString : wrapper
+        {
+            public fullString(wrapper newWrapper)
+            {
+                this.w = newWrapper;
+                num = w.getNum();
+                rounded = w.getRounded();
+                expNotation = w.getExpNotation();
+                fullString = "";
+            }
+        }
+
+        decimal value = 0;
+        Mile suc1 = new Mile();
+        Yard suc2 = new Yard();
+        Feet suc3 = new Feet();
+
+        wrapper wuc1 = new wrapper();
+        wrapper wuc2, wuc3, wuc4;
+
+        public Length_Converter()
+        {
+            InitializeComponent();
+
+            suc2.addSuccessor(suc3);
+            suc1.addSuccessor(suc2);
+        }
+
+        private void emoji_45_Click(object sender, EventArgs e)
+        {
+            value = numbers.Value;
+
+            suc1.Convert_(value);
+
+            switch(A_LIST.SelectedIndex)
+            {
+                case 0:
+                    wuc1.setNum(suc1.getValue());
+                    break;
+                case 1:
+                    wuc1.setNum(suc2.getValue());
+                    break;
+                case 2:
+                    wuc1.setNum(suc3.getValue());
+                    break;
+            }
+
+            wuc2 = new RoundDecimal(wuc1);
+            wuc3 = new expNotation(wuc2);
+            wuc3.generateExpNotation();
+            wuc4 = new fullString(wuc3);
+
+            switch (A_LIST.SelectedIndex)
+            {
+                case 0:
+                    bruhsoundhashtag2.Text = wuc4.generateFullString(A_LIST.SelectedIndex);
+                    //bruhsoundhashtag2.Text = Convert.ToString(suc1.getValue());
+                    break;
+                case 1:
+                    bruhsoundhashtag2.Text = wuc4.generateFullString(A_LIST.SelectedIndex);
+                    //bruhsoundhashtag2.Text = Convert.ToString(suc2.getValue());
+                    break;
+                case 2:
+                    bruhsoundhashtag2.Text = wuc4.generateFullString(A_LIST.SelectedIndex);
+                    //bruhsoundhashtag2.Text = Convert.ToString(suc3.getValue());
+                    break;
+            }
+        }
+    }
+}
